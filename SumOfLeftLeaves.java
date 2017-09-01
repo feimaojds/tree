746d30831c1c9f12705268ce971753c8e1442f75
feimaojds <313061793@qq.com>
2017-09-01 10:50:45 +0800

    Find the sum of all left leaves in a given binary tree.
    思路：
    前序遍历，递归

---
+++ SumOfLeftLeaves.java
@@ -0,0 +1,21 @@
+package leecode;
+
+public class SumOfLeftLeaves {
+
+	
+	
+	 public int sumOfLeftLeaves(TreeNode root) {
+	        
+	    }
+	 
+	 public void search(TreeNode root)
+	
+	
+	
+	class TreeNode {
+		      int val;
+		      TreeNode left;
+		      TreeNode right;
+		      TreeNode(int x) { val = x; }
+		  }
+}
