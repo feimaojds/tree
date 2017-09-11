742092dfae536e7b14f5cf8b31d4aa18e940ed89
feimaojds <313061793@qq.com>
2017-09-11 10:56:02 +0800

    Given two binary trees, write a function to check if they are equal or not.  Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
    思路：
    前序遍历，先比较根节点，一致后再分别比较左右孩子。

---
+++ SameTree.java
@@ -0,0 +1,27 @@
+package leecode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.sun.org.apache.bcel.internal.generic.SIPUSH;
+
+public class SameTree {
+
+	
+	
+public boolean isSameTree(TreeNode p, TreeNode q) {
+         if(p==null && q==null) return true;
+         if(p==null || q==null) return false;
+         if(p.val == q.val)
+        	 return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+         return false;
+    }
+
+
+	class TreeNode {
+		      int val;
+		      TreeNode left;
+		      TreeNode right;
+		     TreeNode(int x) { val = x; }
+		  }
+}
